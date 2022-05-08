edfcb7d3fdcabb88a1bacd0e2f44279a6e03155c
Calvin Low <calsaviour@gmail.com>
2022-05-08 12:13:36 +0800

    WIP post function to download

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+const bodyParser = require("body-parser");
+
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/test", (req, res) => {
   res.json([{
@@ -27,6 +33,30 @@ app.get("/getInfo", async (req, res) => {
   }
   res.json(video)
 })
+
+app.post("/getYoutubeVideoInfo", async (req, res) =>{
+  console.log(req.body);
+  const json = req.body;
+  const youtubeUrl = json.url;
+  console.log(youtubeUrl);
+
+  // Get video info
+  try {
+    video = await ytdl.getInfo(youtubeUrl);
+    console.log(video)
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+
+  res
+    .status(200)
+    .json({
+      message: "Successfully get youtube video info",
+      videoInfo: video,
+      status: 200
+    });
+});
 
 
 app.listen(3001, () => console.log("Server is listening to port 3001"));
