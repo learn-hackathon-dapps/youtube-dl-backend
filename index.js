5e75ed812c171c3656d946f02867e20b2abd1c65
Calvin Low <calsaviour@gmail.com>
2022-05-08 23:48:03 +0800

    Add feature to download video from youtube

--- index.js
+++ index.js
@@ -21,17 +21,28 @@ app.get("/test", (req, res) => {
   }]);
 })
 
-app.get("/getInfo", async (req, res) => {
-  console.log("Getting video from youtube")
+app.post("/downloadVideoFromYoutube", async (req, res) => {
+  console.log("Getting video from youtube");
+  console.log(req.body);
+  const json = req.body;
+  const youtubeUrl = json.url;
+  const videoId = youtubeUrl.split('v=')[1];
+  const filename = videoId + ".mp4";
   // Get video info
   try {
-    video = await ytdl.getInfo('http://www.youtube.com/watch?v=aqz-KE-bpKQ');
+    video = await ytdl(youtubeUrl)
+                    .pipe(fs.createWriteStream(filename));
     console.log(video)
   } catch (e) {
     console.log(e);
     return null;
   }
-  res.json(video)
+  res
+  .status(200)
+  .json({
+    message: "Download from youtube was a success",
+    status: 200
+  });
 })
 
 app.post("/getYoutubeVideoInfo", async (req, res) =>{
