93a0dcbd110c157dbe119bd7fff7e0308340b3b1
Calvin Low <calsaviour@gmail.com>
2022-05-12 18:05:37 +0800

    Web3 storage
    web3 storage for videos
    
    Remove env

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// import { Web3Storage, getFilesFromPath } from 'web3.storage';
+const Web3Storage = require('web3.storage');
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const fs = require('fs');
@@ -11,7 +15,11 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get("/test", (req, res) => {
+
+const token = process.env.TOKEN;
+
+app.get("/test", async (req, res) => {
+  console.log(__dirname);
   res.json([{
     id: 1,
     username: "samsepi0l"
@@ -21,28 +29,41 @@ app.get("/test", (req, res) => {
   }]);
 })
 
+const downloadYoutubeVideo = async (youtubeUrl, filename) => {
+  return ytdl(youtubeUrl).pipe(fs.createWriteStream(filename))
+}
+
 app.post("/downloadVideoFromYoutube", async (req, res) => {
   console.log("Getting video from youtube");
+  console.log(__dirname);
   console.log(req.body);
   const json = req.body;
   const youtubeUrl = json.url;
   const videoId = youtubeUrl.split('v=')[1];
   const filename = videoId + ".mp4";
+  var cid;
   // Get video info
   try {
-    video = await ytdl(youtubeUrl)
-                    .pipe(fs.createWriteStream(filename));
-    console.log(video)
+    video = await downloadYoutubeVideo(youtubeUrl, filename)
+                    .then(async () =>{
+                      const storage = new Web3Storage.Web3Storage( {token} );
+                      const stream = fs.createReadStream(filename)
+                      cid = await storage.put([{ name: filename, stream: () => stream }])
+                      console.log('Content added with CID:', cid)
+    })
   } catch (e) {
     console.log(e);
     return null;
   }
-  res
-  .status(200)
-  .json({
-    message: "Download from youtube was a success",
-    status: 200
-  });
+  if(cid) {
+    res
+    .status(200)
+    .json({
+      message: "Download from youtube was a success",
+      cid: cid,
+      status: 200
+    });
+  }
 })
 
 app.post("/getYoutubeVideoInfo", async (req, res) =>{
